15fa3d0bca25a582eccda98a89efb43906069d92
xieyangyuyue <1483618794@qq.com>
2024-09-24 13:50:12 +0800

    单链表查找

--- List/LinkList.cpp
+++ List/LinkList.cpp
@@ -13,79 +13,76 @@
   LinkList L;//声明一个指向单链表第一个结点的指针
  */
 
-typedef struct LNode {	//定义单链表结点类型
-	int data;			//每个节点存放一个数据元素
-	struct LNode*next;	//指针指向下一个结点
-} LNode, * LinkList;
+typedef struct LNode {
+	// 定义单链表结点类型
+	int data;			// 每个节点存放一个数据元素
+	struct LNode *next; // 指针指向下一个结点
+} LNode, *LinkList;
 
-//判断单链表是否为空
+// 判断单链表是否为空
 bool Empty(LinkList L) {
 	return L->next == nullptr;
 }
 
-//初始化一个单链表(带头结点)
+// 初始化一个单链表(带头结点)
 bool InitList(LinkList &L) {
-	L = (LNode*) malloc(sizeof(LNode));	//分配一个头结点
-	if (Empty(L)) return false;			//内存不够，分配失败
+	L = (LNode *)malloc(sizeof(LNode)); // 分配一个头结点
+	if (Empty(L))
+		return false; // 内存不够，分配失败
 	L->next = NULL;
-	return true;						//头结点之后暂时没有结点
+	return true; // 头结点之后暂时没有结点
 }
 
 // 定义获取链表中第i个元素的函数
-LNode* GetElem(LinkList L, int i) {
-	// 初始化计数器j为1，因为链表的头结点通常不计入元素计数
-	int j = 1;
-	// p指向链表的第一个结点，即头结点的下一个结点
-	LNode* p = L->next;
-	// 如果i为0，表示获取头结点，直接返回头结点指针
-	if (i == 0) return L;
-	// 如果i小于1，表示位置不合法，返回NULL
-	if (i < 1) return NULL;
-	// 当p不为空且j小于i时，继续遍历链表
-	while (p != NULL && j < i) {
-		// p指向下一个结点
-		p = p->next;
-		// 计数器j加1
+LNode *GetElem(LinkList L, int i) {
+	if (i < 0) return NULL;
+	LNode*cur = L; // 指针cur指向当前 L指向头结点
+	int j = 0; // 当前cur指向第几个结点
+	while (cur != NULL && j < i) {
+		// 循环找到第i-1个结点
+		cur = cur->next;
 		j++;
 	}
-	// 返回第i个结点的指针，如果不存在第i个结点，则p将为NULL
-	return p;
+	return cur;
+
 }
 
-//前插操作:在p结点之前插入元素e
-bool InsertPriorNode(LNode* p, int e) {
-	if (p == NULL) return false;
-	LNode* prior = (LNode*)malloc(sizeof(LNode));
-	if (prior == NULL) return false;
+
+
+// 前插操作:在p结点之前插入元素e
+bool InsertPriorNode(LNode *p, int e) {
+	if (p == NULL)
+		return false;
+	LNode *prior = (LNode *)malloc(sizeof(LNode));
+	if (prior == NULL)
+		return false;
 	prior->next = p->next;
 	p->next = prior;
-	//prior结点在p后面 将p结点的数据复制到prior位置，p结点填充e
+	// prior结点在p后面 将p结点的数据复制到prior位置，p结点填充e
 	prior->data = p->data;
 	p->data = e;
 	return true;
 }
 
-//后插操作：在p结点之后插入元素e
-bool InsertNextNode(LNode* p, int e) {
-	if (p == NULL) return false;
-	LNode* s = (LNode*)malloc (sizeof(LNode));
-	if (s == NULL) return false;
-	s->data = e;		//结点保存数据元素e
+// 后插操作：在p结点之后插入元素e
+bool InsertNextNode(LNode *p, int e) {
+	if (p == NULL)
+		return false;
+	LNode *s = (LNode *)malloc(sizeof(LNode));
+	if (s == NULL)
+		return false;
+	s->data = e; // 结点保存数据元素e
 	s->next = p->next;
-	p->next = s;		//将结点s连接到p之后
+	p->next = s; // 将结点s连接到p之后
 	return true;
 }
 
-//在第i个位置插入元素e(带头结点)
+// 在第i个位置插入元素e(带头结点)
 bool ListInsert(LinkList &L, int i, int e) {
-	if (i < 1) return false;
-	LNode *cur = L;	//指针cur指向当前 L指向头结点
-	int j = 0;		//当前cur指向第几个结点
-	while (cur != NULL && j < i - 1) { //循环找到第i-1个结点
-		cur = cur->next;
-		j++;
-	}
-	//后插操作：在p结点之后插入元素e
+	if (i < 1)
+		return false;
+	LNode* cur = GetElem(L, i - 1);//找到第i-1个结点
+	// 后插操作：在p结点之后插入元素e
 	return InsertNextNode(cur, e);
 }
 
@@ -98,16 +95,13 @@ bool ListInsert(LinkList &L, int i, int e) {
  *
  */
 bool ListDelete(LinkList &L, int i, int &e) {
-	if (i < 1) return false;
-	LNode *pre = L;	//指针cur指向当前 L指向头结点
-	int j = 0;		//当前cur指向第几个结点
-	while (pre != NULL && j < i - 1) { //循环找到第i-1个结点
-		pre = pre->next;
-		j++;
-	}
-	//结点位置不对
-	if (!pre || !pre->next) return false;
-	LNode* temp = pre->next;
+	if (i < 1)
+		return false;
+	LNode *pre = GetElem(L, i - 1);//找到第i-1个结点
+	// 结点位置不对
+	if (!pre || !pre->next)
+		return false;
+	LNode *temp = pre->next;
 	e = temp->data;
 	pre->next = temp->next;
 	free(temp);
@@ -118,20 +112,20 @@ bool ListDelete(LinkList &L, int i, int &e) {
  *
  * @param p 删除结点
  */
-bool DeleteNode(LNode* p) {
+bool DeleteNode(LNode *p) {
 
-	if (p == NULL) return false;
-	LNode*temp = p->next;
-	//p结点数据被temp数据覆盖
+	if (p == NULL)
+		return false;
+	LNode *temp = p->next;
+	// p结点数据被temp数据覆盖
 	p->data = temp->data;
-	//将temp链接断开
+	// 将temp链接断开
 	p->next = temp->next;
 	free(temp);
 	return true;
 }
 
 int main() {
-	LinkList L;	//声明一个指向单链表的指针
-	InitList(L);//初始化一个空表
-
+	LinkList L;	 // 声明一个指向单链表的指针
+	InitList(L); // 初始化一个空表
 }
